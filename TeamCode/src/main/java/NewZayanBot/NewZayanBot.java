b0146b0d08a055305d5ab1fbb4cd1857a11b791b
bobthebuilder2332 <mayuheng120@gmail.com>
2025-11-10 15:42:57 -0500

    Create NewZayanBot.java

---
+++ TeamCode/src/main/java/NewZayanBot/NewZayanBot.java
@@ -0,0 +1,107 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+@TeleOp
+public class NewZayanBot extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor fl = hardwareMap.dcMotor.get("FL");
+        DcMotor bl = hardwareMap.dcMotor.get("BL");
+        DcMotor fr = hardwareMap.dcMotor.get("FR");
+        DcMotor br = hardwareMap.dcMotor.get("BR");
+        DcMotor intake = hardwareMap.dcMotor.get("intake");
+        Servo kick = hardwareMap.servo.get("K");
+
+      //  DcMotor turret = hardwareMap.dcMotor.get("turret");
+
+        // Reverse the right side motors. This may be wrong for your setup.
+        // If your robot moves backwards when commanded to go forwards,
+        // reverse the left side instead.
+        // See the note about this earlier on this page.
+        fr.setDirection(DcMotorSimple.Direction.REVERSE);
+        br.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // Retrieve the IMU from the hardware map
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+        // Adjust the orientation parameters to match your robot
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP));
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        imu.initialize(parameters);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double x = gamepad1.left_stick_x; // Remember, Y stick value is reversed
+            double y = -gamepad1.left_stick_y;
+            double rx = -gamepad1.right_stick_x;
+
+            // This button choice was made so that it is hard to hit on accident,
+            // it can be freely changed based on preference.
+            // The equivalent button is start on Xbox-style controllers.
+            if (gamepad1.options) {
+                imu.resetYaw();
+            }
+
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            // Rotate the movement direction counter to the bot's rotation
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+            rotX *= 1.1;  // Counteract imperfect strafing
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double flPower = (rotY + rotX + rx) / denominator;
+            double blPower = (rotY - rotX + rx) / denominator;
+            double frPower = (rotY - rotX - rx) / denominator;
+            double brPower = (rotY + rotX - rx) / denominator;
+
+            fl.setPower(flPower);
+            bl.setPower(blPower);
+            fr.setPower(frPower);
+            br.setPower(brPower);
+            
+            if (gamepad1.right_bumper) {
+                // Right Bumper for Intake (Full Power)
+                intake.setPower(-1);
+            } else if (gamepad1.left_bumper) {
+                // Left Bumper for Outtake/Reverse (Full Reverse Power)
+                intake.setPower(.9);
+            }
+            
+            if (gamepad1.left_trigger > 0) {
+                // Left Trigger for turret on (Full Power)
+               // turret.setPower(1);
+            
+            } else {
+                intake.setPower(0);
+              //  turret.setPower(0);
+            }
+                
+            if (gamepad1.y){
+                kick.setPosition(1);
+            } else {
+                kick.setPosition(0.4);
+            }
+            
+        }
+    }
+}
