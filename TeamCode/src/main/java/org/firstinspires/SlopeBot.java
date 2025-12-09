256e3d72002d22b81fe36ab13330d56f17a9a563
bobthebuilder2332 <mayuheng120@gmail.com>
2025-12-09 14:16:10 -0500

    Update SlopeBot.java

--- TeamCode/src/main/java/org/firstinspires/SlopeBot.java
+++ TeamCode/src/main/java/org/firstinspires/SlopeBot.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class SlopeBot extends LinearOpMode {
@@ -22,30 +23,24 @@ public class SlopeBot extends LinearOpMode {
         DcMotor intake = hardwareMap.dcMotor.get("intake"); // EH M0
         DcMotor flywheel = hardwareMap.dcMotor.get("flywheel"); // EH M1
 
-        CRServo cycle = hardwareMap.crservo.get("cycle"); // CH S0
-        CRServo cycle2 = hardwareMap.crservo.get("cycle2"); // CH S1
+        CRServo center = hardwareMap.crservo.get("center"); // CH S0, the middle servo that spins the balls around
+        CRServo trans = hardwareMap.crservo.get("trans"); // CH S1, the top servo that pushes the balls into the launcher
 
         Servo kick = hardwareMap.servo.get("kick"); // CH S2
-        Servo angle = hardwareMap.servo.get("angle"); // CH S3
+        Servo cycle = hardwareMap.servo.get("cycle"); // CH S3, changes the order of the artifacts
 
         IMU imu = hardwareMap.get(IMU.class, "imu");
         
         // Declaring condition states
         boolean intakeRunning = false; // Intake state
 
-        // Reverse the right side motors. This may be wrong for your setup.
-        // If your robot moves backwards when commanded to go forwards,
-        // reverse the left side instead.
-        // See the note about this earlier on this page.
-        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        // Reverse motors so they spin the correct direction
+        fr.setDirection(DcMotorSimple.Direction.REVERSE);
+        br.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        // Retrieve the IMU from the hardware map
-        // Adjust the orientation parameters to match your robot
-        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
-        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        // Adjust IMU orientation parameters
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.LEFT, RevHubOrientationOnRobot.UsbFacingDirection.UP));
+
         imu.initialize(parameters);
 
         waitForStart();
@@ -53,38 +48,43 @@ public class SlopeBot extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
+
+            // Inputs
             double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
             double x = gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
 
-            // This button choice was made so that it is hard to hit on accident,
-            // it can be freely changed based on preference.
+            // Bot driving
+                double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+                // Rotate the movement direction counter to the bot's rotation
+                double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+                double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+                rotX = rotX * 1.1;  // Counteract imperfect strafing
+
+                // Denominator is the largest motor power (absolute value) or 1
+                // This ensures all the powers maintain the same ratio,
+                // but only if at least one is out of the range [-1, 1]
+                double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+                double frontLeftPower = (rotY + rotX + rx) / denominator;
+                double backLeftPower = (rotY - rotX + rx) / denominator;
+                double frontRightPower = (rotY - rotX - rx) / denominator;
+                double backRightPower = (rotY + rotX - rx) / denominator;
+
+                // Apply settings to move the bot
+                fl.setPower(frontLeftPower);
+                bl.setPower(backLeftPower);
+                fr.setPower(frontRightPower);
+                br.setPower(backRightPower);
+
+
+            // Reset IMU to robot orientation
             // The equivalent button is start on Xbox-style controllers.
             if (gamepad1.options) {
                 imu.resetYaw();
             }
 
-            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-
-            // Rotate the movement direction counter to the bot's rotation
-            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
-            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
-
-            rotX = rotX * 1.1;  // Counteract imperfect strafing
-
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio,
-            // but only if at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-            double frontLeftPower = (rotY + rotX + rx) / denominator;
-            double backLeftPower = (rotY - rotX + rx) / denominator;
-            double frontRightPower = (rotY - rotX - rx) / denominator;
-            double backRightPower = (rotY + rotX - rx) / denominator;
-
-            frontLeftMotor.setPower(frontLeftPower);
-            backLeftMotor.setPower(backLeftPower);
-            frontRightMotor.setPower(frontRightPower);
-            backRightMotor.setPower(backRightPower);
         }
     }
 }
