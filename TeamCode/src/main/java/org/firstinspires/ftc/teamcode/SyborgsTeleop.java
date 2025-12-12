e7aba4e2664c5d7d99dab9e52b14d6ca592bec09
liujustin604 <liujustin604@gmail.com>
2025-12-12 16:56:09 -0500

    reorganize

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SyborgsTeleop.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.tuning.TuningOpModes;
+
+public class SyborgsTeleop extends LinearOpMode {
+	private Limelight3A limelight;
+
+	@Override
+	public void runOpMode() throws InterruptedException {
+		telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+		MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+
+		waitForStart();
+
+		while (opModeIsActive()) {
+			drive.setDrivePowers(new PoseVelocity2d(
+					new Vector2d(
+							-gamepad1.left_stick_y,
+							-gamepad1.left_stick_x
+					),
+					-gamepad1.right_stick_x
+			));
+
+			drive.updatePoseEstimate();
+
+			Pose2d pose = drive.localizer.getPose();
+			telemetry.addData("x", pose.position.x);
+			telemetry.addData("y", pose.position.y);
+			telemetry.addData("heading (deg)", Math.toDegrees(pose.heading.toDouble()));
+			telemetry.update();
+
+			TelemetryPacket packet = new TelemetryPacket();
+			packet.fieldOverlay().setStroke("#3F51B5");
+			Drawing.drawRobot(packet.fieldOverlay(), pose);
+			FtcDashboard.getInstance().sendTelemetryPacket(packet);
+		}
+	}
+}
