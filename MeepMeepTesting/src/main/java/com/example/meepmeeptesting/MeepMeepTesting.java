3450e44415daa2c29d2996688a2087d0a3855562
liujustin604 <liujustin604@gmail.com>
2026-01-10 12:19:05 -0500

    update meepmeep

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,13 +1,31 @@
 package com.example.meepmeeptesting;
 
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.InstantAction;
 import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.SleepAction;
+import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.DriveShim;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
+import java.util.function.Supplier;
+
 public class MeepMeepTesting {
+	public DriveShim drive;
+	public Supplier<Action> startIntakeAction = () -> new InstantAction(() -> {
+	});
+
+	public Supplier<Action> shootAction = () -> new InstantAction(() -> {
+	});
+
+	public interface Shooter {
+		Action stopIntakeAction();
+	}
+
+	public int obeliskID;
+	public Shooter shooter = () -> new InstantAction(() -> {});
 
 	public static void main(String[] args) {
 		System.setProperty("sun.java2d.opengl", "true");
@@ -19,34 +37,77 @@ public class MeepMeepTesting {
 				.setConstraints(50, 50, Math.toRadians(180), Math.toRadians(180), 14)
 				.build();
 
-		double shootingTime = 5;
 
-		myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(60, 12, Math.toRadians(180)))
-				.strafeToLinearHeading(new Vector2d(-10, 10), Math.toRadians(130))
-				.stopAndAdd(new SleepAction(shootingTime))
+		myBot.getDrive().setPoseEstimate(new Pose2d(60, 12, Math.toRadians(180)));
+		MeepMeepTesting testing = new MeepMeepTesting(23, myBot.getDrive());
+		myBot.runAction(testing.getAutonAction());
 
-				.strafeToLinearHeading(new Vector2d(20, 26), Math.toRadians(105))
-				.splineToSplineHeading(new Pose2d(32, 48, Math.toRadians(90)), Math.toRadians(90))
-				.strafeToLinearHeading(new Vector2d(-10, 10), Math.toRadians(130))
-				.stopAndAdd(new SleepAction(shootingTime))
-
-				.strafeToSplineHeading(new Vector2d(0, 20), Math.toRadians(105))
-				.splineToLinearHeading(new Pose2d(10, 48, Math.toRadians(90)), Math.toRadians(90))
-				.strafeToLinearHeading(new Vector2d(-10, 10), Math.toRadians(130))
-				.stopAndAdd(new SleepAction(shootingTime))
-
-				.strafeToSplineHeading(new Vector2d(-12, 24), Math.toRadians(115))
-				.splineToLinearHeading(new Pose2d(-12, 48, Math.toRadians(90)), Math.toRadians(90))
-				.setReversed(true)
-				.strafeToLinearHeading(new Vector2d(-10, 10), Math.toRadians(130))
-				.stopAndAdd(new SleepAction(shootingTime))
-
-				.build());
-
-		meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_OFFICIAL)
+		meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_DARK)
 				.setDarkMode(true)
 				.setBackgroundAlpha(0.95f)
 				.addEntity(myBot)
 				.start();
+	}
+
+	public MeepMeepTesting(int obeliskID, DriveShim drive) {
+		this.obeliskID = obeliskID;
+		this.drive = drive;
+
+	}
+
+	public Action getAutonAction() {
+		Action autonAction = new SequentialAction(runPreloaded(), runCycleGPP(), runCyclePGP(), runCyclePPG());
+		if (obeliskID == 22) {
+			autonAction = new SequentialAction(runPreloaded(), runCyclePGP(), runCycleGPP(), runCyclePPG());
+		} else if (obeliskID == 23) {
+			autonAction = new SequentialAction(runPreloaded(), runCyclePPG(), runCycleGPP(), runCyclePGP());
+		}
+		return autonAction;
+	}
+
+	public Action runPreloaded() {
+
+		return drive.actionBuilder(drive.getPoseEstimate())
+				// shoot preloaded
+				.strafeToLinearHeading(new Vector2d(-10, 10), Math.toRadians(130))
+				.stopAndAdd(shootAction.get())
+				.build();
+	}
+
+	public Action runCycleGPP() {
+		return drive.actionBuilder(new Pose2d(-10, 10, Math.toRadians(130)))
+				// first cycldrive.localizer.getPose(), poseMape
+				.strafeToLinearHeading(new Vector2d(20, 26), Math.toRadians(90))
+				.afterDisp(10, startIntakeAction.get())
+				.splineToSplineHeading(new Pose2d(32, 48, Math.toRadians(90)), Math.toRadians(90))
+				.afterDisp(10, shooter.stopIntakeAction())
+				.strafeToLinearHeading(new Vector2d(-10, 10), Math.toRadians(130))
+				.stopAndAdd(shootAction.get())
+				.build();
+	}
+
+	public Action runCyclePGP() {
+		return drive.actionBuilder(new Pose2d(-10, 10, Math.toRadians(130)))
+				// second cycle
+				.strafeToSplineHeading(new Vector2d(0, 20), Math.toRadians(105))
+				.afterDisp(10, startIntakeAction.get())
+				.splineToLinearHeading(new Pose2d(10, 48, Math.toRadians(90)), Math.toRadians(90))
+				.afterDisp(10, shooter.stopIntakeAction())
+				.strafeToLinearHeading(new Vector2d(-10, 10), Math.toRadians(130))
+				.stopAndAdd(shootAction.get())
+				.build();
+	}
+
+	public Action runCyclePPG() {
+		return drive.actionBuilder(new Pose2d(-10, 10, Math.toRadians(130)))
+				// third cycle
+				.strafeToSplineHeading(new Vector2d(-11, 24), Math.toRadians(115))
+				.afterDisp(10, startIntakeAction.get())
+				.splineToLinearHeading(new Pose2d(-12, 48, Math.toRadians(90)), Math.toRadians(90))
+				.afterDisp(10, shooter.stopIntakeAction())
+				.setReversed(true)
+				.strafeToLinearHeading(new Vector2d(-10, 10), Math.toRadians(130))
+				.stopAndAdd(shootAction.get())
+				.build();
 	}
 }
