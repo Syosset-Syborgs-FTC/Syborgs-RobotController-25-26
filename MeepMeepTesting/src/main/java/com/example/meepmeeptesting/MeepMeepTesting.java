54074e16037d75d9e772781864c9929470a8fc77
liujustin604 <liujustin604@gmail.com>
2026-01-10 00:38:18 -0500

    update meepmeep trajectory

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,32 +1,46 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SleepAction;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
+
 	public static void main(String[] args) {
 		System.setProperty("sun.java2d.opengl", "true");
+
 		MeepMeep meepMeep = new MeepMeep(800);
 
 		RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
 				// Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-				.setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+				.setConstraints(50, 50, Math.toRadians(180), Math.toRadians(180), 14)
 				.build();
 
-		myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(60, 12, Math.toRadians(180)))
+		double shootingTime = 5;
 
-				.splineToLinearHeading(new Pose2d(-10, 10, Math.toRadians(130)), Math.toRadians(135))
-				.strafeToLinearHeading(new Vector2d(36, 36), Math.toRadians(90))
+		myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(60, 12, Math.toRadians(180)))
+				.strafeToLinearHeading(new Vector2d(-10, 10), Math.toRadians(130))
+				.stopAndAdd(new SleepAction(shootingTime))
+
+				.strafeToLinearHeading(new Vector2d(20, 26), Math.toRadians(105))
+				.splineToSplineHeading(new Pose2d(32, 48, Math.toRadians(90)), Math.toRadians(90))
+				.strafeToLinearHeading(new Vector2d(-10, 10), Math.toRadians(130))
+				.stopAndAdd(new SleepAction(0.5))
+
+				.strafeToSplineHeading(new Vector2d(0, 20), Math.toRadians(105))
+				.splineToLinearHeading(new Pose2d(10, 48, Math.toRadians(90)), Math.toRadians(90))
+				.strafeToLinearHeading(new Vector2d(-10, 10), Math.toRadians(130))
+				.stopAndAdd(new SleepAction(0.5))
+
+				.strafeToSplineHeading(new Vector2d(-12, 24), Math.toRadians(115))
+				.splineToLinearHeading(new Pose2d(-12, 48, Math.toRadians(90)), Math.toRadians(90))
 				.setReversed(true)
-				.splineToLinearHeading(new Pose2d(-10, 10, Math.toRadians(130)), Math.toRadians(135))
-				.strafeToSplineHeading(new Vector2d(12, 36), Math.toRadians(90))
-				.splineToLinearHeading(new Pose2d(-10, 10, Math.toRadians(130)), Math.toRadians(135))
-				.splineToLinearHeading(new Pose2d(-12, 36, Math.toRadians(90)), Math.toRadians(90))
-				.setReversed(true)
-				.splineToLinearHeading(new Pose2d(-10, 10, Math.toRadians(130)), Math.toRadians(130))
+				.strafeToLinearHeading(new Vector2d(-10, 10), Math.toRadians(130))
+				.stopAndAdd(new SleepAction(shootingTime))
+
 				.build());
 
 		meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_OFFICIAL)
